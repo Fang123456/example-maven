ddea0e3c37276e2c48828cfd342318e781c4518e
Fang123456 <2797037168@qq.com>
2016-01-06 09:58:49 +0800

    Maven
    一个简单的maven项目的创建

---
+++ src/main/java/com/k/example/maven/example_maven/App.java
@@ -0,0 +1,13 @@
+package com.k.example.maven.example_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
